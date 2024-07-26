05cccf4b15b53095f65a86fcb61b281698387fa0
crowdbotics-apps <team@crowdbotics.com>
2024-07-26 20:06:09 +0000

    Update models

--- backend/expanseshare_48883/wsgi.py
+++ backend/expanseshare_48883/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "expanseshare_48883.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expanseshare_48883.settings")
 
 application = get_wsgi_application()
